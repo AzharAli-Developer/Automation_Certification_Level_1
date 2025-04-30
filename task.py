3faef277e1f040c989dabd2ef24b5bb2128475a2
AzharAli-Developer <azharalisp059@gmail.com>
2025-04-30 19:55:04 +0500

    add new functions in project

--- task.py
+++ task.py
@@ -1,13 +1,67 @@
+import os
+from RPA.PDF import PDF
+from RPA.HTTP import HTTP
+from RPA.Excel.Files import Files
 from RPA.Browser.Selenium import Selenium
+
 
 class Task:
     def __init__(self):
+        self.pdf = PDF()
+        self.http = HTTP()
+        self.file = Files()
         self.browser = Selenium()
 
     def open_website(self):
         self.browser.open_available_browser("https://robotsparebinindustries.com/")
 
+    def log_in_webssite(self):
+        self.browser.wait_until_element_is_visible('//input[@id="username"]',timeout=10)
+        self.browser.input_text('//input[@id="username"]',"maria")
+        self.browser.input_text('//input[@id="password"]',"thoushallnotpass")
+        self.browser.click_button('//button[@type="submit"]')
+
+    def fill_and_submit_sales_form(self,data):
+        self.browser.wait_until_element_is_visible('//input[@id="firstname"]')
+        self.browser.input_text('//input[@id="firstname"]', text=data['First Name'])
+        self.browser.input_text('//input[@id="lastname"]', text=data['Last Name'])
+        self.browser.select_from_list_by_value('//select[@id="salestarget"]',f'{data["Sales Target"]}')
+        self.browser.input_text('//input[@id="salesresult"]', text=data['Sales'])
+        self.browser.click_button('//button[@type="submit"]')
+
+    def download_excel_file(self):
+        excel_file_path = os.path.join(os.getcwd(), 'output/SalesData.xlsx')
+        self.http.download(url='https://robotsparebinindustries.com/SalesData.xlsx',target_file=excel_file_path,  overwrite=True)
+
+    def read_excel_and_fill_form(self):
+        excel_file_path = os.path.join(os.getcwd(), 'output/SalesData.xlsx')
+        self.file.open_workbook(excel_file_path)
+        excel_data = self.file.read_worksheet_as_table("data", header=True)
+        self.file.close_workbook()
+        for row in excel_data:
+            self.fill_and_submit_sales_form(row)
+
+    def take_sales_summary_screenshot(self):
+        excel_file_path = os.path.join(os.getcwd(), 'output/sales_summary.png')
+        self.browser.capture_element_screenshot('//div[@role="alert"]', filename=excel_file_path)
+
+    def create_pdf_sales_result(self):
+        self.browser.wait_until_element_is_visible('//div[@id="sales-results"]',timeout=10)
+        pdf_path = os.path.join(os.getcwd(), 'output/Sales_Result.pdf')
+        self.pdf.get
+
+
+    def log_out_website(self):
+        self.browser.wait_until_element_is_visible('//button[@id="logout"]',timeout=10)
+        self.browser.click_button('//button[@id="logout"]')
+        ...
 
 
 task = Task()
 task.open_website()
+task.log_in_webssite()
+task.download_excel_file()
+task.read_excel_and_fill_form()
+task.take_sales_summary_screenshot()
+task.create_pdf_sales_result()
+task.log_out_website()
