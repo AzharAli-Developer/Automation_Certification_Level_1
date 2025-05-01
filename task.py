f8bbdc2046eb88822a3b4dfd4dff2892c11cd792
AzharAli-Developer <azharalisp059@gmail.com>
2025-05-01 11:58:23 +0500

    add docstring

--- task.py
+++ task.py
@@ -6,6 +6,11 @@ from RPA.Browser.Selenium import Selenium
 
 
 class Task:
+    """First level of Web Automation Certificates.
+
+    The python class open the website and login. After login, download the excel file and fill the register form Auto,
+    take screenshot and create pdf of Sales Data.
+    """
     def __init__(self):
         self.pdf = PDF()
         self.http = HTTP()
@@ -13,15 +18,18 @@ class Task:
         self.browser = Selenium()
 
     def open_website(self):
+        """open website"""
         self.browser.open_available_browser("https://robotsparebinindustries.com/")
 
     def log_in_webssite(self):
+        """login website"""
         self.browser.wait_until_element_is_visible('//input[@id="username"]',timeout=10)
         self.browser.input_text('//input[@id="username"]',"maria")
         self.browser.input_text('//input[@id="password"]',"thoushallnotpass")
         self.browser.click_button('//button[@type="submit"]')
 
     def fill_and_submit_sales_form(self,data):
+        """fill register form"""
         self.browser.wait_until_element_is_visible('//input[@id="firstname"]')
         self.browser.input_text('//input[@id="firstname"]', text=data['First Name'])
         self.browser.input_text('//input[@id="lastname"]', text=data['Last Name'])
@@ -30,10 +38,12 @@ class Task:
         self.browser.click_button('//button[@type="submit"]')
 
     def download_excel_file(self):
+        """download excel file"""
         excel_file_path = os.path.join(os.getcwd(), 'output/SalesData.xlsx')
         self.http.download(url='https://robotsparebinindustries.com/SalesData.xlsx',target_file=excel_file_path,  overwrite=True)
 
     def read_excel_and_fill_form(self):
+        """read excel file and fill register form"""
         excel_file_path = os.path.join(os.getcwd(), 'output/SalesData.xlsx')
         self.file.open_workbook(excel_file_path)
         excel_data = self.file.read_worksheet_as_table("data", header=True)
@@ -42,19 +52,21 @@ class Task:
             self.fill_and_submit_sales_form(row)
 
     def take_sales_summary_screenshot(self):
+        """take sales summary screenshot"""
         excel_file_path = os.path.join(os.getcwd(), 'output/sales_summary.png')
         self.browser.capture_element_screenshot('//div[@role="alert"]', filename=excel_file_path)
 
     def create_pdf_sales_result(self):
+        """create pdf of sales result"""
         self.browser.wait_until_element_is_visible('//div[@id="sales-results"]',timeout=10)
+        html_data = self.browser.get_element_attribute('//div[@id="sales-results"]','innerHTML')
         pdf_path = os.path.join(os.getcwd(), 'output/Sales_Result.pdf')
-        self.pdf.get
-
+        self.pdf.html_to_pdf(html_data, pdf_path)
 
     def log_out_website(self):
+        """logout website"""
         self.browser.wait_until_element_is_visible('//button[@id="logout"]',timeout=10)
         self.browser.click_button('//button[@id="logout"]')
-        ...
 
 
 task = Task()
